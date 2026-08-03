57afe13106819c307bee31c6bd22dfe122fe4f48
nasir21b@gmail.com <nasir21b@gmail.com>
2026-08-03 14:02:44 +0600

    feat: implement proxy file for RBAC

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,110 @@
-import { NextResponse } from "next/server";
+import { JwtPayload } from "jsonwebtoken";
+import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getNewAccessToken } from "./services/refreshToken";
+import { jwtUtils } from "./utils/jwt"; // আপনার ইউটিল পাথ
 
-export function proxy(request: NextRequest) {
+const AUTH_ROUTES = ["/login", "/register"];
 
+const PUBLIC_ROUTES = ["/", "/properties"];
 
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const cookieStore = await cookies();
 
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
-    
+  let decodedAccessToken = accessToken
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
+    : null;
+
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
+
+  if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    const result = await getNewAccessToken();
+
+    if (result?.success) {
+      const newAccessToken = result.data.accessToken;
+
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+
+  let userRole: string | null = null;
+  if (!decodedAccessToken?.success) {
+    cookieStore.delete("accessToken");
+  } else if (decodedAccessToken?.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
+  }
+
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else if (userRole === "LANDLORD") {
+      return NextResponse.redirect(new URL("/landlord-dashboard", request.url));
+    } else if (userRole === "TENANT") {
+      return NextResponse.redirect(new URL("/tenant-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  //Authorization (Role Based Access Control - RBAC)
+
+  // ADMIN Only
+  if (pathname.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  // LANDLORD Only
+  if (pathname.startsWith("/landlord-dashboard") && userRole !== "LANDLORD") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  // TENANT Only
+  if (pathname.startsWith("/tenant-dashboard") && userRole !== "TENANT") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  // TENANT Only Payment Pages
+  if (
+    (pathname.startsWith("/payment/success") ||
+      pathname.startsWith("/payment/cancel")) &&
+    userRole !== "TENANT"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin-dashboard/:path*", "/landlord-dashboard/:path*", "/tenant-dashboard/:path*"],
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
 };
